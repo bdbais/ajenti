5e116d944b1292b17d4be4c8149d28cc6ba5a68b
bdbais <bais@bais.info>
2015-11-12 15:29:35 +0100

    Add proxy support
    A simple proxy support for ppl living behind a proxy

--- scripts/install-raspbian.sh
+++ scripts/install-raspbian.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# PROXY SETTINGS uncomment following lines to use a proxy
+
+# export http_proxy="http://user:password@proxy.corp.com:8080"
+# pip_proxy='--proxy=" + $http_proxy + '"'
+
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -16,9 +22,9 @@ apt-get update
 echo ':: Installing packages'
 apt-get install -y ajenti 
 apt-get install -y python-pip python-dev libevent-dev 
-pip install -U gevent 
-pip install greenlet==dev 
-pip install psutil --upgrade 
+pip install -U gevent $pip_proxy
+pip install greenlet==dev $pip_proxy
+pip install psutil --upgrade $pip_proxy
 /etc/init.d/ajenti restart 
 
 echo ':: Done! Open https://<address>:8000 in browser'
